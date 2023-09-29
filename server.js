981e13a445dad6991df444e468c5778f4782e76e
vikas <vickyvsvirat@gmail.com>
2023-09-29 18:46:10 +0530

    server problem solved

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ import { connectDb } from "./data/database.js";
 
 connectDb();
 
-app.listen(() => {
+app.listen(process.env.PORT, () => {
     console.log(`Server is working on port: ${process.env.PORT} in ${process.env.NODE_ENV} mode`);
 });
