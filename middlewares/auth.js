9496fc1d5a943a2db3a878e0b74c6a807743ff08
vikas <vickyvsvirat@gmail.com>
2023-10-02 16:41:47 +0530

    auth

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,12 +1,11 @@
 import { User } from "../models/user.js";
 import jwt from "jsonwebtoken";
+import ErrorHandler from "./error.js";
 
 export const isAuthenticated = async (req, res, next) => {
     const { token } = req.cookies;
-    if (!token) return res.status(404).json({
-        success: false,
-        message: "Login First"
-    });
+
+    if (!token) return next(new ErrorHandler("Login First", 404));
 
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
